1c43059321900d21b3eab82c3954f6381096648f
Tyler Gregorcyk <tyler@gregorcyk.com>
2026-09-02 23:45:45 -0500

    Fix `test_shader.ps.hlsl` build error
    ```
    shader_compiler/shader_compiler_slang.odin(160:5) _compile_slang_wgsl:
    Slang Error: FAIL (0x800040
    05):
    error[E30701]: type 'uint' is not valid for system value semantic
    'SV_IsFrontFace'; expected 'bool'
     --> test_shader.ps:6:39
      |
    6 | float4 ps_main(RV_Varyings vars, uint frontface : SV_IsFrontFace) :
    SV_Target {
      |                                       ^
    --'
    ```
    
    Signed-off-by: Tyler Gregorcyk <tyler@gregorcyk.com>

--- examples/data/test_shader.ps.hlsl
+++ examples/data/test_shader.ps.hlsl
@@ -3,7 +3,7 @@
 RV_RESOURCE_SLOT(2, Texture2DArray tex);
 RV_SAMPLER_SLOT(0, SamplerState smp);
 
-float4 ps_main(RV_Varyings vars, uint frontface : SV_IsFrontFace) : SV_Target {
+float4 ps_main(RV_Varyings vars, bool frontface : SV_IsFrontFace) : SV_Target {
     float3 normal = normalize(bool(frontface) ? vars.normal : -vars.normal);
     float4 col = vars.add_col + vars.col * tex.Sample(smp, float3(vars.uv, float(vars.tex_slice)));
     col.rgb = 1.0 - col.rgb;
